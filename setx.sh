1ddf85fb306062d32a9961e00d3a8452dc5a2e43
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-25 20:49:02 +0300

    Create setx.sh

---
+++ setx.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "
+
+_        
+| |/ /_|  \/  | _
+| ' // _ \ |\/| |/ _ \
+| . \  / |  | | (_) |
+|_|\_\___|_|  |_|\___/
+
+     𓍹 Visit @rruuurr for help 𓍻
+"
+
+python3 -m pyUltroid
